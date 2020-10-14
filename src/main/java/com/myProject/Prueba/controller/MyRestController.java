acc2dafa952edc6c78ed361d3bb6bb13b02b262b
Rubén Romero Planas <rromero@cifpfbmoll.eu>
2020-10-14 21:14:28 +0200

    1

--- src/main/java/com/myProject/Prueba/controller/MyRestController.java
+++ src/main/java/com/myProject/Prueba/controller/MyRestController.java
@@ -1,6 +1,12 @@
 package com.myProject.Prueba.controller;
 
 import com.myProject.Prueba.data.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 
@@ -13,5 +19,10 @@ public class MyRestController {
         aux.setName(name);
         aux.setMark(8);
         return aux;
+    }
+
+    @GetMapping("/createStudent")
+    public List<Student> createStudent(@RequestParam(value = "q", defaultValue = "1") Integer numStudents) {
+        ArrayList<Student> arrData 
     }
 }
